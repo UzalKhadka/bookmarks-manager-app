c29621ee650f5bf0b219818efae68494ac5dfb5b
Ujjal <khadkauzal@gmail.com>
2021-04-09 20:50:22 +0545

    completed get request

---
+++ backend/data/data.js
@@ -0,0 +1,268 @@
+const data = [
+  {
+    _id: '1',
+    username: 'admin',
+    name: 'Admin User',
+    password: '12345',
+    bookmarks: [
+      {
+        _id: '1',
+        name: 'Youtube',
+        link: 'www.youtube.com',
+        category: 'misc',
+      },
+      {
+        _id: '2',
+        name: 'Instagram',
+        link: 'www.instagram.com',
+        category: 'misc',
+      },
+      {
+        _id: '3',
+        name: 'Facebook',
+        link: 'www.facebook.com',
+        category: 'misc',
+      },
+      {
+        _id: '4',
+        name: 'Github',
+        link: 'www.github.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '5',
+        name: 'Basecamp',
+        link: 'www.basecamp.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '6',
+        name: 'Stack Overflow',
+        link: 'www.stackoverflow.com',
+        category: 'Padhai',
+      },
+    ],
+  },
+  {
+    _id: '2',
+    username: 'babu.ram',
+    name: 'Babu Ram',
+    password: '22222',
+    bookmarks: [
+      {
+        _id: '1',
+        name: 'Youtube',
+        link: 'www.youtube.com',
+        category: 'misc',
+      },
+      {
+        _id: '2',
+        name: 'Instagram',
+        link: 'www.instagram.com',
+        category: 'misc',
+      },
+      {
+        _id: '3',
+        name: 'Facebook',
+        link: 'www.facebook.com',
+        category: 'misc',
+      },
+      {
+        _id: '4',
+        name: 'Github',
+        link: 'www.github.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '5',
+        name: 'Basecamp',
+        link: 'www.basecamp.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '6',
+        name: 'Stack Overflow',
+        link: 'www.stackoverflow.com',
+        category: 'Padhai',
+      },
+    ],
+  },
+  {
+    _id: '3',
+    username: 'shankar.dev',
+    name: 'Shankar Dev',
+    password: '33333',
+    bookmarks: [
+      {
+        _id: '1',
+        name: 'Youtube',
+        link: 'www.youtube.com',
+        category: 'misc',
+      },
+      {
+        _id: '2',
+        name: 'Instagram',
+        link: 'www.instagram.com',
+        category: 'misc',
+      },
+      {
+        _id: '3',
+        name: 'Facebook',
+        link: 'www.facebook.com',
+        category: 'misc',
+      },
+      {
+        _id: '4',
+        name: 'Github',
+        link: 'www.github.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '5',
+        name: 'Basecamp',
+        link: 'www.basecamp.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '6',
+        name: 'Stack Overflow',
+        link: 'www.stackoverflow.com',
+        category: 'Padhai',
+      },
+    ],
+  },
+  {
+    _id: '4',
+    username: 'rohit.shetty',
+    name: 'Rohit Shetty',
+    password: '44444',
+    bookmarks: [
+      {
+        _id: '1',
+        name: 'Youtube',
+        link: 'www.youtube.com',
+        category: 'misc',
+      },
+      {
+        _id: '2',
+        name: 'Instagram',
+        link: 'www.instagram.com',
+        category: 'misc',
+      },
+      {
+        _id: '3',
+        name: 'Facebook',
+        link: 'www.facebook.com',
+        category: 'misc',
+      },
+      {
+        _id: '4',
+        name: 'Github',
+        link: 'www.github.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '5',
+        name: 'Basecamp',
+        link: 'www.basecamp.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '6',
+        name: 'Stack Overflow',
+        link: 'www.stackoverflow.com',
+        category: 'Padhai',
+      },
+    ],
+  },
+  {
+    _id: '5',
+    username: 'kamal.hasan',
+    name: 'Kamal Hasan',
+    password: '55555',
+    bookmarks: [
+      {
+        _id: '1',
+        name: 'Youtube',
+        link: 'www.youtube.com',
+        category: 'misc',
+      },
+      {
+        _id: '2',
+        name: 'Instagram',
+        link: 'www.instagram.com',
+        category: 'misc',
+      },
+      {
+        _id: '3',
+        name: 'Facebook',
+        link: 'www.facebook.com',
+        category: 'misc',
+      },
+      {
+        _id: '4',
+        name: 'Github',
+        link: 'www.github.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '5',
+        name: 'Basecamp',
+        link: 'www.basecamp.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '6',
+        name: 'Stack Overflow',
+        link: 'www.stackoverflow.com',
+        category: 'Padhai',
+      },
+    ],
+  },
+  {
+    _id: '6',
+    username: 'ganesh.gurung',
+    name: 'Ganesh Gurung',
+    password: '66666',
+    bookmarks: [
+      {
+        _id: '1',
+        name: 'Youtube',
+        link: 'www.youtube.com',
+        category: 'misc',
+      },
+      {
+        _id: '2',
+        name: 'Instagram',
+        link: 'www.instagram.com',
+        category: 'misc',
+      },
+      {
+        _id: '3',
+        name: 'Facebook',
+        link: 'www.facebook.com',
+        category: 'misc',
+      },
+      {
+        _id: '4',
+        name: 'Github',
+        link: 'www.github.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '5',
+        name: 'Basecamp',
+        link: 'www.basecamp.com',
+        category: 'Padhai',
+      },
+      {
+        _id: '6',
+        name: 'Stack Overflow',
+        link: 'www.stackoverflow.com',
+        category: 'Padhai',
+      },
+    ],
+  },
+]
+
+module.exports = data
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const bookmarks = require('./data/bookmarks')
-const users = require('./data/users')
+const data = require('./data/data')
 
 const app = express()
 
@@ -8,22 +7,27 @@ app.get('/', (req, res) => {
   res.send('Backend API is running...')
 })
 
-app.get('/api/bookmarks/', (req, res) => {
-  res.json(bookmarks)
-})
-
-app.get('/api/bookmarks/:id', (req, res) => {
-  const bookmark = bookmarks.find((p) => p._id === req.params.id)
-  res.json(bookmark)
-})
-
 app.get('/api/users/', (req, res) => {
-  res.json(users)
+  res.json(data)
 })
 
 app.get('/api/users/:user', (req, res) => {
-  const user = users.find((p) => p.username === req.params.user)
+  const user = data.find((p) => p.username === req.params.user)
   res.json(user)
+})
+
+app.get('/api/users/:user/bookmarks/', (req, res) => {
+  const user = data.find((p) => p.username === req.params.user)
+  bookmarks = user.bookmarks
+  res.json(bookmarks)
+})
+
+app.get('/api/users/:user/bookmarks/:id', (req, res) => {
+  const user = data.find((p) => p.username === req.params.user)
+  bookmarks = user.bookmarks
+
+  const bookmark = bookmarks.find((p) => p._id === req.params.id)
+  res.json(bookmark)
 })
 
 app.listen(5000, console.log('Server running on port 5000'))
